962ebaa4614268b124f1dadbed7d260216e9325b
nmtyksample <nmtyk.sample@gmail.com>
2024-09-05 15:19:56 +0900

    v4

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 from geopy.geocoders import Nominatim
-from google.cloud import translate_v2 as translate
+from googletrans import Translator
 from geopy.exc import GeocoderTimedOut, GeocoderUnavailable
 from geopy.distance import geodesic
 import numpy as np
@@ -10,7 +10,7 @@ import time
 import io
 
 # タイトルの設定
-st.title("あいのりタクシーアプリv5")
+st.title("あいのりタクシーアプリ4")
 
 # ファイルアップロード
 uploaded_file = st.file_uploader("名前と住所が記載されたExcelファイルをアップロードしてください", type=["xlsx"])
@@ -21,11 +21,12 @@ if uploaded_file:
     st.write("アップロードされたデータ:")
     st.write(df)
     
-    # Google Cloud Translation APIクライアントの設定
+    # Google Translate APIを使用して住所を英語に翻訳
+    translator = Translator()
+
     def translate_address(address):
-        client = translate.Client()
-        result = client.translate(address, target_language='en', source_language='ja')
-        return result['translatedText']
+        translated = translator.translate(address, src='ja', dest='en')
+        return translated.text
 
     # ジオコーダの設定
     geolocator = Nominatim(user_agent="taxi_allocation", timeout=10)  # タイムアウトを10秒に設定
