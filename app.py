e500dcf941ddf91dc5c6a2881f05a8d35ba4fb3d
nmtyksample <nmtyk.sample@gmail.com>
2024-09-13 00:19:45 +0900

    ta30

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import re
 from datetime import datetime
 
 # タイトルの設定
-st.title("あいのりタクシーアプリ_タクとも29🚕👫")
+st.title("あいのりタクシーアプリ_タクとも30🚕👫")
 
 # 出発地点の入力フォーム (デフォルトで渋谷のNHKの住所を設定)
 start_address = st.text_input("出発地点を入力してください", placeholder="東京都渋谷区神南2-2-1 NHK放送センター")
@@ -60,7 +60,7 @@ def is_valid_coordinates(coords):
     if coords is None:
         return False
     # 緯度と経度を正しい順序で出力
-    latitude, longitude = coords[0], coords[1]
+    latitude, longitude = coords[1], coords[0]
     st.write(f"Checking coordinates: 緯度: {latitude}, 経度: {longitude}")  # 正しい順序でデバッグ出力
     return -90 <= latitude <= 90 and -180 <= longitude <= 180
 
